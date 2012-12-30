57fd40afa4f63826cde39856186f7b867cdb4bff
James Fraser <wulfgar.pro@gmail.com>
2012-12-30 20:36:47 +1030

    added manually copy of oh-my-zsh

---
+++ .oh-my-zsh/lib/correction.zsh
@@ -0,0 +1,13 @@
+setopt correct_all
+
+alias man='nocorrect man'
+alias mv='nocorrect mv'
+alias mysql='nocorrect mysql'
+alias mkdir='nocorrect mkdir'
+alias gist='nocorrect gist'
+alias heroku='nocorrect heroku'
+alias ebuild='nocorrect ebuild'
+alias hpodder='nocorrect hpodder'
+
+# ignore git config
+alias git='nocorrect git'

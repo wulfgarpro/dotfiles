84bdc6391a75b48f56aaeec19a9dae4d5ce9a5fa
James Fraser <wulfgar.pro@gmail.com>
2014-07-11 10:41:13 +1000

    latest work

--- code/lcthw/ex8.c
+++ code/lcthw/ex8.c
@@ -3,7 +3,8 @@
 int main(int argc, char *argv[])
 {
     int areas[] = {10, 12, 13, 14, 20};
-    char name[] = "Zed";
+    //char name[] = "Zed";
+    char *name = "Zed";
     char full_name[] = {
         'Z', 'e', 'd',
          ' ', 'A', '.', ' ',

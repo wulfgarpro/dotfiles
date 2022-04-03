40e8aa2b96caf3f4d6fcb0dacbfdb0b49b192032
James Fraser <wulfgar.pro@gmail.com>
2022-04-03 12:27:21 +1000

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,67 +1,58 @@
 #!/usr/bin/env bash
 #set -x
 
-# Assume Ubuntu + Gnome-terminal as default system,
-# WSL install does not include fonts or terminal theme.
+# Assumptions - Archlinux with a well defined $PATH.
 
 if [ "$1" == "--help" ]; then
-    echo "./install [--wsl]"
+    echo "./install [--noback]"
     exit 1
 fi
 
-echo "###########################################"
-echo "++ Installing git and cloning submodules ++"
-echo "###########################################"
-sudo apt-get install git
-git submodule update --init --recursive
+echo "############################################"
+echo "++ Installing base and cloning submodules ++"
+echo "############################################"
+sudo pacman -Syuq --noconfirm
+sudo pacman -Sq --noconfirm --needed \
+    alacritty                       \
+    base-devel                      \
+    bat                             \
+    firefox                         \
+    git                             \
+    meld                            \
+    neovim                          \
+    tmux
+sudo pacman -Sqcc --noconfirm
+git submodule update --init
 
-echo "###########################################"
-echo "++ Installing system dependencies ++"
-echo "###########################################"
-deps="vim vim-gtk3 fontconfig zsh python3 python3-pip"
-if [ "$1" == "--wsl" ]; then
-    sudo apt-get install "$deps"
-else
-    sudo apt-get install "$deps" #dconftools gconf2
-fi
-
-echo "###########################################"
-echo "++ Installing python modules ++"
-echo "###########################################"
-python3 -m pip install pip --upgrade
-python3 -m pip install --user virtualenvwrapper 
-
-echo "###########################################"
+echo "############################################"
 echo "++ Configuring environment ++"
-echo "###########################################"
-declare -a resources=(".gvimrc" ".oh-my-zsh" ".vimrc" ".zshrc" ".vim")
+echo "############################################"
+declare -a resources=(
+    "config/alacritty"
+    "config/nvim"
+    "config/tmux"
+    "config/zsh"
+    "gitconfig"
+    "zshrc"
+)
 for i in "${resources[@]}"
 do
-    mv "$HOME"/"$i" "$HOME"/"${i}"_backup_"$(date +%H%M%S)"
-    ln -fv -ns "$PWD"/"$i" "$HOME"/"$i" 
+    # This is a destructive operation.
+    ln -fv -ns "$PWD/$i" "$HOME/.$i"
 done
 
-echo "###########################################"
+echo "############################################"
 echo "++ Installing vim plugins with Vundle ++"
-echo "###########################################"
+echo "############################################"
 echo "vim +PluginInstall +qall"
 vim +PluginInstall +qall
+# TODO(wulfgarpro): Install CoC extensions.
 
-if [ "$1" != "--wsl" ]; then
-    echo "###########################################"
-    echo "++ Installing Powerline patched fonts ++"
-    echo "###########################################"
-    "$PWD"/.fonts/install.sh
+echo "############################################"
+echo "++ Installing themes ++"
+echo "############################################"
+# TODO(wulfgarpro): Install themes (Dracula).
 
-    echo "###########################################"
-    echo "++ Installing theme for gnome-terminal ++"
-    echo "###########################################"
-    # Permission issue?
-    chmod +x "$PWD"/.onehalf-gnome-terminal-setup/onehalfdark.sh
-    "$PWD"/.onehalf-gnome-terminal-setup/onehalfdark.sh
-fi
-
-echo "###############################################"
+echo "############################################"
 echo "++ Done... run /bin/zsh ++"
-echo "###############################################"
-
+echo "############################################"

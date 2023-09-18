4edef05b006bbb48b453c9ec99ee7d82abd40067
James Fraser <wulfgar.pro@gmail.com>
2023-09-19 06:43:02 +1000

    Add Vim options

--- config/custom/init.lua
+++ config/custom/init.lua
@@ -1,18 +1,23 @@
--- local autocmd = vim.api.nvim_create_autocmd
-
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
-
 vim.opt.colorcolumn = "80"
 
--- Treat some files as other files
+-- Treat some files as other files.
 vim.cmd [[ 
   autocmd BufRead,BufNewFile Containerfile set filetype=dockerfile
 ]]
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false -- don't fold by default
+-- Don't fold by default.
+vim.opt.foldenable = false
+
+-- Decrease update time.
+vim.opt.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience.
+vim.completeopt = "menuone,noselect"
+
+-- Highlight extra whitespace.
+vim.cmd [[
+  autocmd BufEnter,BufRead * match Error /\s\+$/
+  autocmd BufWritePre * silent! call matchdelete(//)
+]]

f4096bfb2a347d115f8ba36db49d48db1bfd8bac
James Fraser <wulfgar.pro@gmail.com>
2023-12-12 08:25:21 +1100

    config/wezterm: Add transparency

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,8 +1,10 @@
 return {
-	color_scheme = "Dracula (Official)",
-	tab_bar_at_bottom = true,
-	use_fancy_tab_bar = false,
-	window_decorations = "RESIZE",
-	font_size = 13.0,
-	warn_about_missing_glyphs = false,
+  color_scheme = "Dracula (Official)",
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  window_decorations = "RESIZE",
+  font_size = 13.0,
+  warn_about_missing_glyphs = false,
+  window_background_opacity = 0.95,
+
 }

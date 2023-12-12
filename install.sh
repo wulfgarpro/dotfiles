c185c3c8169bc05a14843eb69fb1fef8f4deff6e
James Fraser <wulfgar.pro@gmail.com>
2023-12-12 11:25:03 +1100

    config/nvim: Install luarocks for luacheck

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ set -u
 # well-defined $PATH.
 
 if [ "${1-}" == "--help" ]; then
-  echo "./install"
-  exit 1
+	echo "./install"
+	exit 1
 fi
 
 read -n 1 -s -r -p "Press any key to continue..."
@@ -21,77 +21,78 @@ echo "++ Installing base and cloning submodules ++"
 echo "############################################"
 sudo pacman -Syuq --noconfirm
 sudo pacman -Sq --noconfirm --needed \
-  base-devel \
-  curl \
-  fd \
-  firefox \
-  gedit \
-  gedit-plugins \
-  git \
-  git-lfs \
-  grim \
-  jq \
-  lazygit \
-  light \
-  meld \
-  neovim \
-  net-tools \
-  pamixer \
-  pipewire-pulse \
-  polkit \
-  pyenv \
-  python-pillow \
-  python-pynvim \
-  ranger \
-  ripgrep \
-  slurp \
-  sway \
-  swaybg \
-  swayidle \
-  swaylock \
-  tree \
-  ttf-dejavu \
-  ttf-fira-sans \
-  ttf-font-awesome \
-  ttf-jetbrains-mono-nerd \
-  unzip \
-  waybar \
-  wezterm \
-  wget \
-  wofi \
-  xplr \
-  xorg-xwayland \
-  zig
+	base-devel \
+	curl \
+	fd \
+	firefox \
+	gedit \
+	gedit-plugins \
+	git \
+	git-lfs \
+	grim \
+	jq \
+	lazygit \
+	light \
+	luarocks \
+	meld \
+	neovim \
+	net-tools \
+	pamixer \
+	pipewire-pulse \
+	polkit \
+	pyenv \
+	python-pillow \
+	python-pynvim \
+	ranger \
+	ripgrep \
+	slurp \
+	sway \
+	swaybg \
+	swayidle \
+	swaylock \
+	tree \
+	ttf-dejavu \
+	ttf-fira-sans \
+	ttf-font-awesome \
+	ttf-jetbrains-mono-nerd \
+	unzip \
+	waybar \
+	wezterm \
+	wget \
+	wofi \
+	xplr \
+	xorg-xwayland \
+	zig
 sudo pacman -Sqcc --noconfirm
 
 # If `paru` is not installed, install from AUR:
 # `git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si`
 paru -Sq --noconfirm --needed \
-  swaync
+	swaync
 paru -Sqcc --noconfirm
 
 echo "############################################"
 echo "++ Configuring environment ++"
 echo "############################################"
 declare -a resources=(
-  "config/gtk-3.0"
-  "config/fontconfig"
-  "config/nvim"
-  "config/ranger"
-  "config/sway"
-  "config/systemd"
-  "config/waybar"
-  "config/wezterm"
-  "config/wofi"
-  "config/zsh"
-  "gitconfig"
-  "p10k.zsh"
-  "zshrc"
+	"config/gtk-3.0"
+	"config/fontconfig"
+	"config/nvim"
+	"config/ranger"
+	"config/sway"
+	"config/systemd"
+	"config/waybar"
+	"config/wezterm"
+	"config/wofi"
+	"config/zsh"
+	"gitconfig"
+	"p10k.zsh"
+	"zshrc"
 )
 for i in "${resources[@]}"; do
-  # The below is a destructive operation!
-  rm -rf "$HOME/.$i"
-  ln -sfT "$(pwd)/$i" "$HOME/.$i"
+	# The below is a destructive operation!
+	rm -rf "$HOME/.$i"
+	ln -sfT "$(pwd)/$i" "$HOME/.$i"
 done
 
 # Reload and enable the `set-bing-wallpaper` systemd timer.

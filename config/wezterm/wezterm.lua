e805b897a7b2035bd01cd861d0f4c2225663694a
James Fraser <wulfgar.pro@gmail.com>
2024-01-07 06:35:37 +1100

    config/wezterm: Use Tokyonight colors

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,10 +1,9 @@
 return {
-  color_scheme = "Dracula (Official)",
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
-  font_size = 13.0,
-  warn_about_missing_glyphs = false,
-  window_background_opacity = 0.95,
-
+	color_scheme = "tokyonight_moon",
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+	window_decorations = "RESIZE",
+	font_size = 13.0,
+	warn_about_missing_glyphs = false,
+	-- window_background_opacity = 0.95,
 }

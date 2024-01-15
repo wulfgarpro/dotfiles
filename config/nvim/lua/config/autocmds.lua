e89e2a835a2a3fe2dbb5f64944a60961a105574f
James Fraser <wulfgar.pro@gmail.com>
2024-01-16 10:11:01 +1100

    config/nvim: Fix relative colorcolumn

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -4,6 +4,16 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "gitcommit" },
   command = [[
+    set colorcolumn=+0
     set colorcolumn+=50
   ]]
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype ~= "gitcommit" and vim.bo.textwidth > 0 then
+      vim.wo.colorcolumn = "+0"
+    end
+  end
 })

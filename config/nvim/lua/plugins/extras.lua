1f90f27d06c8abe23163799e45875d8c5bdbf337
James Fraser <wulfgar.pro@gmail.com>
2025-01-13 10:29:17 +1100

    config/nvim: Latest config

--- config/nvim/lua/plugins/extras.lua
+++ config/nvim/lua/plugins/extras.lua
@@ -1,21 +1,27 @@
 return {
-  -- coding
-  { import = "lazyvim.plugins.extras.coding.copilot" },
+  --
+  -- ai
+  { import = "lazyvim.plugins.extras.ai.copilot" },
 
   -- formatting
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = "lazyvim.plugins.extras.formatting.black" },
 
   -- lang
+  { import = "lazyvim.plugins.extras.lang.ansible" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.lang.cmake" },
   { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.java" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.zig" },
 
   -- ui
-  { import = "lazyvim.plugins.extras.ui.mini-animate" },
+  -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 }
--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -6,6 +6,7 @@ return {
       "shellcheck",
       "bash-language-server",
       "hadolint",
+      "selene",
     },
   },
 }

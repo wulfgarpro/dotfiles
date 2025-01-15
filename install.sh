69ac6cc725e11c82a000f227f0e14aa7ef267b69
James Fraser <wulfgar.pro@gmail.com>
2025-01-15 11:13:56 +1100

    install.sh: Simplified install script

--- install.sh
+++ install.sh
@@ -1,107 +1,120 @@
 #!/usr/bin/env bash
+
 set -e
 set -o pipefail
 set -u
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Assumptions - an initial Arch Linux install with internet access and a
 # well-defined $PATH.
 
-if [ "${1-}" == "--help" ]; then
-	echo "./install"
-	exit 1
-fi
+# if [ "${1-}" == "--help" ]; then
+#   echo "./install"
+#   exit 1
+# fi
 
 read -n 1 -s -r -p "Press any key to continue..."
+echo
 
 git submodule update --init
 
-echo -e "\n"
-echo "############################################"
-echo "++ Installing base and cloning submodules ++"
-echo "############################################"
-sudo pacman -Syuq --noconfirm
-sudo pacman -Sq --noconfirm --needed \
-	base-devel \
-	curl \
-	fd \
-	firefox \
-	gedit \
-	gedit-plugins \
-	git \
-	git-lfs \
-	grim \
-	jq \
-	lazygit \
-	luarocks \
-	meld \
-	neovim \
-	net-tools \
-	noto-fonts \
-	noto-fonts-cjk \
-	noto-fonts-extra \
-	pamixer \
-	pipewire-pulse \
-	polkit \
-	pyenv \
-	python-pillow \
-	python-pynvim \
-	ranger \
-	ripgrep \
-	slurp \
-	sway \
-	swaybg \
-	swayidle \
-	swaylock \
-	tree \
-	ttf-font-awesome \
-	ttf-jetbrains-mono \
-	ttf-jetbrains-mono-nerd \
-	ttf-nerd-fonts-symbols \
-	ttf-nerd-fonts-symbols-mono \
-	unzip \
-	waybar \
-	wezterm \
-	wget \
-	wofi \
-	xplr \
-	xorg-xwayland \
-	zig
-sudo pacman -Sqcc --noconfirm
+# echo -e "\n"
+# echo "############################################"
+# echo "++ Installing base and cloning submodules ++"
+# echo "############################################"
+
+# sudo pacman -Syuq --noconfirm
+# sudo pacman -Sq --noconfirm --needed \
+#   base-devel \
+#   curl \
+#   fd \
+#   firefox \
+#   gedit \
+#   gedit-plugins \
+#   git \
+#   git-lfs \
+#   grim \
+#   jq \
+#   lazygit \
+#   luarocks \
+#   meld \
+#   neovim \
+#   net-tools \
+#   noto-fonts \
+#   noto-fonts-cjk \
+#   noto-fonts-extra \
+#   pamixer \
+#   pipewire-pulse \
+#   polkit \
+#   pyenv \
+#   python-pillow \
+#   python-pynvim \
+#   ranger \
+#   ripgrep \
+#   slurp \
+#   sway \
+#   swaybg \
+#   swayidle \
+#   swaylock \
+#   tree \
+#   ttf-font-awesome \
+#   ttf-jetbrains-mono \
+#   ttf-jetbrains-mono-nerd \
+#   ttf-nerd-fonts-symbols \
+#   ttf-nerd-fonts-symbols-mono \
+#   unzip \
+#   waybar \
+#   wezterm \
+#   wget \
+#   wofi \
+#   xplr \
+#   xorg-xwayland \
+#   zig
+# sudo pacman -Sqcc --noconfirm
 
 # If `paru` is not installed, install from AUR:
 # `git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si`
-paru -Sq --noconfirm --needed \
-  light
-paru -Sqcc --noconfirm
+# paru -Sq --noconfirm --needed \
+#   light
+# paru -Sqcc --noconfirm
 
 echo "############################################"
-echo "++ Configuring environment ++"
+echo "++ Installing dotfiles ++ "
 echo "############################################"
+
 declare -a resources=(
-	"config/gtk-3.0"
-	"config/fontconfig"
-	"config/nvim"
-	"config/ranger"
-	"config/sway"
-	"config/systemd"
-	"config/waybar"
-	"config/wezterm"
-	"config/wofi"
-	"config/zsh"
-	"gitconfig"
-	"p10k.zsh"
-	"zshrc"
+  # "config/gtk-3.0"
+  # "config/fontconfig"
+  "config/nvim"
+  "config/ranger"
+  # "config/sway"
+  # "config/systemd"
+  # "config/waybar"
+  "config/wezterm"
+  # "config/wofi"
+  "config/zsh"
+  "gitconfig"
+  "p10k.zsh"
+  "zshrc"
+  "zsh_aliases"
 )
+
 for i in "${resources[@]}"; do
-	# The below is a destructive operation!
-	rm -rf "$HOME/.$i"
-	ln -sfT "$(pwd)/$i" "$HOME/.$i"
+  destination="${HOME}/.$i"
+  if [ -e "${destination}" ]; then
+    read -r -p "${destination} already exists. Do you want to overwrite? Y/N " overwrite
+    if [[ "${overwrite}" =~ ^[yY] ]]; then
+      rm -rf "${HOME}/.$i"
+    else
+      continue
+    fi
+  fi
+  ln -sfT "$SCRIPT_DIR/$i" "${HOME}/.$i"
 done
 
 # Reload and enable the `set-bing-wallpaper` systemd timer.
-systemctl --user daemon-reload
-systemctl --user enable --now set-bing-wallpaper.timer
+# systemctl --user daemon-reload
+# systemctl --user enable --now set-bing-wallpaper.timer
 
-echo "############################################"
-echo "++ Done... run exec /bin/zsh ++"
-echo "############################################"
+exec zsh

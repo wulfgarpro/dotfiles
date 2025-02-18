2dc343747975d2ee9164253e51f12fb2bc5e57be
James Fraser <wulfgar.pro@gmail.com>
2025-02-18 12:32:18 +1100

    wezterm: Added leader key + Windows config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -40,12 +40,28 @@ table.insert(config.keys, {
 	action = wezterm.action.MoveTabRelative(1),
 })
 
+-- Leader key is `CTRL+b`.
+config.leader = {
+	key = "b",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
+}
+
+-- Leader activated keys.
+
+-- Copy mode.
+table.insert(config.keys, {
+	key = "[",
+	mods = "LEADER",
+	action = wezterm.action.ActivateCopyMode,
+})
+
 -- Close current pane
--- table.insert(config.keys, {
--- 	key = "w",
--- 	mods = "CTRL",
--- 	action = wezterm.action.CloseCurrentPane({ confirm = true }),
--- })
+table.insert(config.keys, {
+	key = "w",
+	mods = "LEADER|CTRL",
+	action = wezterm.action.CloseCurrentPane({ confirm = true }),
+})
 
 config.color_scheme = "Tokyo Night Storm"
 config.tab_bar_at_bottom = false
@@ -54,5 +70,11 @@ config.window_decorations = "RESIZE"
 config.font_size = 11.0
 config.warn_about_missing_glyphs = true
 config.enable_scroll_bar = true
+config.switch_to_last_active_tab_when_closing_tab = true
+
+-- Windows configs.
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.font = wezterm.font("Cascadia Code PL")
+end
 
 return config

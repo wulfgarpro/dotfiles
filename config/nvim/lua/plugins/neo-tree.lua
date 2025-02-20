311d472246f41a17847e2e185ccac136aa3c63a1
James Fraser <wulfgar.pro@gmail.com>
2025-02-21 09:14:23 +1100

    nvim: Show hidden files in Neotree

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -2,5 +2,10 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = { position = "current" },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
   },
 }

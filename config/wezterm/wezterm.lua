090d7b599c042302b0030fa6a230cd0bf0786a07
James Fraser <wulfgar.pro@gmail.com>
2025-02-27 16:27:09 +1100

    wezterm: Use config builder

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,10 +1,55 @@
 local wezterm = require("wezterm")
-local config = {}
-local launch_menu = {}
 
-config.keys = {}
+local config = wezterm.config_builder()
 
--- Change tab
+-- Set `LEADER` mod to `CTRL+b`.
+config.leader = {
+	key = "b",
+	mods = "CTRL",
+	timeout_milliseconds = 2000,
+}
+
+config.keys = {
+	-- Rename tab title.
+	{
+		key = "e",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	-- Move current tab left.
+	{
+		key = "LeftArrow",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.MoveTabRelative(-1),
+	},
+	-- Move current tab right.
+	{
+		key = "RightArrow",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.MoveTabRelative(1),
+	},
+	-- Activate copy mode.
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	-- Close current pane.
+	{
+		key = "w",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+}
+
+-- Change to tab number.
 for i = 1, 8 do
 	table.insert(config.keys, {
 		key = tostring(i),
@@ -12,57 +57,6 @@ for i = 1, 8 do
 		action = wezterm.action.ActivateTab(i - 1),
 	})
 end
-
--- Rename tab
-table.insert(config.keys, {
-	key = "e",
-	mods = "CTRL|SHIFT",
-	action = wezterm.action.PromptInputLine({
-		description = "Enter new name for tab",
-		action = wezterm.action_callback(function(window, pane, line)
-			if line then
-				window:active_tab():set_title(line)
-			end
-		end),
-	}),
-})
-
--- Move tab left
-table.insert(config.keys, {
-	key = "LeftArrow",
-	mods = "ALT|SHIFT",
-	action = wezterm.action.MoveTabRelative(-1),
-})
-
--- Move tab right
-table.insert(config.keys, {
-	key = "RightArrow",
-	mods = "ALT|SHIFT",
-	action = wezterm.action.MoveTabRelative(1),
-})
-
--- Leader key is `CTRL+b`.
-config.leader = {
-	key = "b",
-	mods = "CTRL",
-	timeout_milliseconds = 2000,
-}
-
--- Leader activated keys.
-
--- Copy mode.
-table.insert(config.keys, {
-	key = "[",
-	mods = "LEADER",
-	action = wezterm.action.ActivateCopyMode,
-})
-
--- Close current pane
-table.insert(config.keys, {
-	key = "w",
-	mods = "LEADER|CTRL",
-	action = wezterm.action.CloseCurrentPane({ confirm = true }),
-})
 
 config.color_scheme = "Tokyo Night Storm"
 config.tab_bar_at_bottom = false
@@ -76,8 +70,8 @@ config.command_palette_font_size = 11.0
 config.command_palette_bg_color = "#24283b"
 config.command_palette_fg_color = "#c0caf5"
 
--- Windows configs.
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+if wezterm.target_triple:find("windows") then
+	-- Windows specific configs go here.
 end
 
 return config

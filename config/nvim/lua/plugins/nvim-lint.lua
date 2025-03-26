1e29a2420ce8d9fef88b2a154046bebdfd12ba88
James Fraser <wulfgar.pro@gmail.com>
2025-03-26 16:58:03 +1100

    nvim: Specify config file for nvim-lint

--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -1,6 +1,15 @@
+local HOME = os.getenv("HOME")
+
 return {
   {
     "mfussenegger/nvim-lint",
     optional = true,
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", HOME .. "/.markdownlint-cli2.yaml", "--" },
+        },
+      },
+    },
   },
 }

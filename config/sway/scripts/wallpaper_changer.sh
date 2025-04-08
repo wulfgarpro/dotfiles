f6d25a49e172e1ced231d10be11915d60b0d7b4c
James Fraser <wulfgar.pro@gmail.com>
2025-04-08 15:54:43 +1000

    sway: Updated configs

--- config/sway/scripts/wallpaper_changer.sh
+++ config/sway/scripts/wallpaper_changer.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-is_swaybg_ServerExist=$(ps -ef | grep -m 1 swaybg | grep -v "grep" | wc -l)
-if [ "$is_swaybg_ServerExist" = "0" ]; then
-  echo "swaybg_server not found" >/dev/null 2>&1
-#	exit;
-elif [ "$is_swaybg_ServerExist" = "1" ]; then
-  killall swaybg
-fi
-
-wallpaper=$(find ~/Pictures/Wallpapers/. -name "*.png" | shuf -n1)
-echo $wallpaper
-swaybg -i $wallpaper -m fill &
-read -r -p "Copy over wallpaper.png? Y/N " set_wallpaper
-if [[ "${set_wallpaper}" =~ ^[yY] ]]; then
-  cp $wallpaper ~/.config/sway/wallpaper.png
-fi
+wallpaper=$(find ~/Pictures/Wallpapers/. \( -name "*.png" -o -name "*.jpg" \) | shuf -n1)
+echo "$wallpaper"
+swaybg -i "$wallpaper" -m fill &
+# read -r -p "Copy over wallpaper.png? Y/N " set_wallpaper
+# if [[ "${set_wallpaper}" =~ ^[yY] ]]; then
+#   cp $wallpaper ~/.config/sway/wallpaper.png
+# fi

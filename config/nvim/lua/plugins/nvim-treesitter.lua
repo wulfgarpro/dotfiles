12b845248591188373177310954237a01cbcad6c
James Fraser <wulfgar.pro@gmail.com>
2025-07-07 09:05:42 +1000

    Fix markview load order

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "OXY2DEV/markview.nvim" },
+  lazy = false,
   opts = function(_, opts)
     -- Extend the default `ensure_installed` list.
     vim.list_extend(opts.ensure_installed, {

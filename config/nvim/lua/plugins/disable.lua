6f720e40c233062cefb33b3206f7c053ec20387e
James Fraser <wulfgar.pro@gmail.com>
2025-09-23 12:41:28 +1000

    Update nvim

--- config/nvim/lua/plugins/disable.lua
+++ config/nvim/lua/plugins/disable.lua
@@ -1,5 +1,5 @@
 return {
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "markdown-preview.nvim", enabled = false },
   { "render-markdown.nvim", enabled = false },
 }

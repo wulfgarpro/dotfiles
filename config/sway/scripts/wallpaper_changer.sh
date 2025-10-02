e93309613a369cd93bec6a34b538dde1c730ae87
James Fraser <wulfgar.pro@gmail.com>
2025-10-03 09:08:19 +1000

    Update wallpaper

--- config/sway/scripts/wallpaper_changer.sh
+++ config/sway/scripts/wallpaper_changer.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-wallpaper=$(find ~/Pictures/Wallpapers/. \( -name "*.png" -o -name "*.jpg" \) | shuf -n1)
+
+wallpaper=""
+if [[ -n "$1" ]]; then
+  wallpaper="$1"
+else
+  wallpaper=$(find ~/Pictures/Wallpapers/. \( -name "*.png" -o -name "*.jpg" \) | shuf -n1)
+fi
+
 echo "$wallpaper"
 swaybg -i "$wallpaper" -m fill &
-# read -r -p "Copy over wallpaper.png? Y/N " set_wallpaper
-# if [[ "${set_wallpaper}" =~ ^[yY] ]]; then
-#   cp $wallpaper ~/.config/sway/wallpaper.png
-# fi
+
+read -r -p "Copy over wallpaper.jpd? Y/N " set_wallpaper
+if [[ "${set_wallpaper}" =~ ^[yY] ]]; then
+  cp "$wallpaper" ~/.config/sway/wallpaper.jpg
+fi
